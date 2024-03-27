17d7f462c74674cf2173de299813277b8282c207
Renan Soares Souza <renansoares.oficiall@gmail.com>
2024-03-27 16:33:37 -0300

    Criando um chat em python Criptografado EM BETA

--- Scripts_Tools/chatEncrypt_beta.py
+++ Scripts_Tools/chatEncrypt_beta.py
@@ -1,8 +1,8 @@
 import socket
 import threading
-
+import re
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
+final = ''
 class Chat:
 	def __init__(self,conType,crypto,ip,port = 0):
 		self.conType = conType
@@ -15,19 +15,23 @@ def CHAT(tipo):
 	if tipo == 1:
 		while status == 0:
 			conversa = input('')
+			conversa = Crypto(crypto,conversa)
 			env = ((conversa).encode())
 			s.sendall(env)
 	if tipo == 2:
 		while status == 0:
 			resp = s.recv(1024)
-			print(resp.decode('utf-8'))
+			resp = Descrypto(resp.decode('utf-8'),crypto)
+			print(resp)
 	if tipo == 3:
 		while True:
 			resp = con.recv(1024)
-			print(resp.decode('utf-8'))
+			resp = Descrypto(resp.decode('utf-8'),crypto)
+			print(resp)
 	if tipo == 4:
 		while True:
 			conversa = input('')
+			conversa = Crypto(crypto,conversa)
 			env = ((conversa).encode())
 			con.sendall(env)
 fala = threading.Thread(target=CHAT, args=[2])
@@ -37,7 +41,34 @@ conversas = threading.Thread(target=CHAT, args=[4])
 
 ip = '127.0.0.1'
 tipo = int(input('Selecione o que você vai ser:\r\n1-Client\r\n2-Server\r\n--> '))
-crypto = input('chave criptográfica:(8 caracteres ou menos)\r\n--> ')
+crypto = int(input('chave criptográfica:(8 caracteres)\r\n--> '))
+dict = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+def Crypto(crypto,mesg):
+	result = []
+	global dict
+	msg = mesg
+	for x in msg:
+		if x == " ":
+			converted = ' '
+			result.append(converted)
+		else:
+			converted = (dict.index(x) + crypto) %26
+			result.append(dict[converted])
+	final = ''.join(result)
+	return final
+def Descrypto(msg,crypto):
+	global dict
+	back = []
+	for x in msg:
+		if x == " ":
+			origin = ' '
+			back.append(origin)
+		else:
+			converted = (dict.index(x) - crypto) %26
+			back.append(dict[converted])
+	final = ''.join(back)
+	return final
 if (tipo == 1):
 	ip = input('qual o Ip do servidor:\r\n--> ');
 	port = int(input('qual a porta de conexão:\r\n--> '))
